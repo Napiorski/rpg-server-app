998c155bbdb254b75365d10950ef3b27394594b2
Napiorski <michaeldnapiorski@gmail.com>
2023-02-13 22:43:03 -0800

    finished almost all the props

--- src/schemas/character.schema.ts
+++ src/schemas/character.schema.ts
@@ -1,8 +1,23 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 @Schema()
 export class Character {
-  @Prop({ type: String, required: true, maxlength: 30 })
+  @Prop({ type: String, required: true, MAX_LENGTH: 30 })
   name: string;
+
+  @Prop({ type: Number, required: true, MIN_VALUE: 1, MAX_VALUE: 300 })
+  age: number;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 5 })
+  weight: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 7 })
+  height: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 500 })
+  backstory: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 300 })
+  alliesAndOrganizations: string;
 
   @Prop({ type: String, required: true })
   race: string;
@@ -13,16 +28,25 @@ export class Character {
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 100 })
   level: number;
 
+  @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10000 })
+  experience: number;
+
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 1000 })
-  hp: number;
+  maxHp: number;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 30 })
+  condition: string;
+
+  @Prop({ type: Number, required: false, MIN_VALUE: 0, MAX_VALUE: 1000 })
+  currentHp: number;
 
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
-  ac: number;
+  armorClass: number;
 
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
   init: number;
 
-  @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
+  @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 120 })
   speed: number;
 
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
@@ -30,6 +54,9 @@ export class Character {
 
   @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
   proficiencyBonus: number;
+
+  @Prop({ type: Number, required: true, MIN_VALUE: 0, MAX_VALUE: 10 })
+  perception: number;
 
   @Prop({ type: Number, required: true, MIN_VALUE: 1, MAX_VALUE: 10 })
   hitDice: number;
@@ -42,5 +69,46 @@ export class Character {
 
   @Prop({ type: Number, required: true, MIN_VALUE: 1, MAX_VALUE: 99 })
   skills: number;
+
+  @Prop({ type: String, required: true })
+  language: string;
+
+  @Prop({ type: String, required: false })
+  equippedItems: string;
+
+  @Prop({ type: String, required: true })
+  background: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 300 })
+  personality: string;
+
+  @Prop({ type: String, required: false, MAX_LENGTH: 300 })
+  ideals: string;
+
+  @Prop({ type: String, required: false, MAX_LENGTH: 300 })
+  bonds: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 300 })
+  flaws: string;
+
+  @Prop({ type: String, required: true })
+  alignment: string;
+
+  @Prop({ type: String, required: false })
+  attackItem: string;
+
+  @Prop({ type: String, required: false })
+  attackItemBonus: string;
+
+  @Prop({ type: String, required: false })
+  attackItemDamageType: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 400 })
+  attackNotes: string;
+
+  @Prop({ type: String, required: true, MAX_LENGTH: 400 })
+  featuresAndTraits: string;
+
+  //Need Spell Props and Proficiency Props
 }
 export const StudentSchema = SchemaFactory.createForClass(Character);
